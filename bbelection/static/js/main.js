2b7ca20acd0f87727d02085bc8908ab42a18ef68
Alexander G. Yang <spark8402@rogers.com>
2019-06-20 15:41:33 -0400

    assignment done

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-});
+
+    
+        axios
+        .get("https://bb-election-api.herokuapp.com/")
+        .then(response => {
+            console.log(response.data);
+            
+            const electionCandidate = document.querySelector('#election');
+            
+            response.data.candidates.forEach((e) => {
+            const electionData = document.createElement("li");
+            electionData.innerHTML = (`${e.name}: ${e.votes} votes`);
+            electionCandidate.appendChild(electionData);    
+            })
+
+        })
+    })
+
+
+
+
+
+
+
+
+
+
